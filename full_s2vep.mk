4e9235673276c73bed79ae5d9729e6fcd7585979
KINGbabasula <kingbabasula@gmail.com>
2013-10-23 21:49:28 +0200

    Update full_s2vep.mk

--- full_s2vep.mk
+++ full_s2vep.mk
@@ -4,9 +4,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/s2vep/s2vep.mk)
 
-# Inherit common device configuration
-$(call inherit-product, device/samsung/galaxys2plus-common/device.mk)
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := s2vep
 PRODUCT_NAME := full_s2vep
