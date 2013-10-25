bc237571a426dfde0d215d96f637e86df8cdead7
KINGbabasula <kingbabasula@gmail.com>
2013-10-25 20:39:36 +0200

    Fix typo

--- full_s2vep.mk
+++ full_s2vep.mk
@@ -6,7 +6,7 @@ $(call inherit-product, device/samsung/s2vep/s2vep.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := s2vep
-PRODUCT_NAME := cm_s2vep
+PRODUCT_NAME := full_s2vep
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I9105P
 PRODUCT_MANUFACTURER := samsung
